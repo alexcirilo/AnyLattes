3b571f4d7016290c5df85cce82f9934c7a81ed2c
alexcirilo <c.alex.cirilo@gmail.com>
2022-08-07 23:58:07 -0300

    Salvar no banco os dados dos projetos

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ from flask import Flask, redirect, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import xml.etree.ElementTree as ET
-#from flask_mysqldb import MySQL
-#from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
 
+user= 'b96e08051c345f'
+pwd= '2503c6ba'
+host= 'us-cdbr-east-06.cleardb.net'
+database= 'heroku_34fb507d853ce4f'
 
 # configurações de upload arquivos
 
@@ -26,8 +29,11 @@ else:
 
 app = Flask(__name__)
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Qwer#1234@localhost/lattes4web'
-#db = SQLAlchemy(app)
+try:
+    db = mysql.connector.connect(user=user,password= pwd,host=host, database= database)
+    print(db)
+except:
+    print("not connect")
 
 
 @app.route('/connection')
@@ -44,31 +50,6 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER  # atribuição da pasta
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-'''
-class Projetos(db.Model):
-    __tablename__ = 'projetos'
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nome = db.Column(db.String(255))
-    titulo = db.Column(db.Text)
-    ano_inicio = db.Column(db.DateTime)
-    natureza = db.Column(db.String(100))
-    coordenador = db.Column(db.String(255))
-    financiamento = db.Column(db.String(255))
-    integrantes = db.Column(db.Text)
-
-    def __init__(self, nome, titulo, ano_inicio, natureza, coordenador, financiamento, integrantes):
-        self.nome = nome
-        self.titulo = titulo
-        self.ano_inicio = ano_inicio
-        self.natureza = natureza
-        self.coordenador = coordenador
-        self.financiamento = financiamento
-        self.integrantes = integrantes
-
-
-db.create_all()
-'''
 
 @app.route("/")
 def index():
@@ -154,8 +135,9 @@ def projetos():
                     integrantes.append(v.attrib['NOME-COMPLETO'])
                     #print(v.attrib['NOME-COMPLETO'],v.attrib['FLAG-RESPONSAVEL'], coordenador)
                     if v.attrib['FLAG-RESPONSAVEL'] == "SIM" and coordenador == "":
-                        #coordenador = v.attrib['NOME-COMPLETO']
-                        coordenadores.append(v.attrib['NOME-COMPLETO'])
+                        coordenadores = v.attrib['NOME-COMPLETO']
+                        
+                        
                         # print(coordenador)
                         #worksheet.write(temp, 4, coordenador)
 
@@ -168,101 +150,19 @@ def projetos():
             if entra:
                 fin = ', '.join(set(financiamento))
                 # print(fin)
-        print(coord)
-        print(projeto)
-        print(ano_inicio)
-        print(natureza)
-        print(coordenadores)
-        print(integra)
-        print(fin)
-    '''
-        for m in range(0, len(curriculos)):
-            tree2 = ET.parse(curriculos[m])
-            root2 = tree2.getroot()
-
-            coord = []
-
-            for t2 in root2.iter('DADOS-GERAIS'):  # Imprimir nome do professor
-                coord.append(t2.attrib['NOME-COMPLETO'])
-
-            integrantes = []
-            financiamento = []
-            for t in root2.iter('PARTICIPACAO-EM-PROJETO'):
-
-                entra = False
-                coordenador = ""
-
-                for v in t.iter():
-                    if v.tag == 'PROJETO-DE-PESQUISA' and not v.attrib['ANO-FIM']:
-                        #print(v.attrib)
-                        projeto = v.attrib['NOME-DO-PROJETO']
-                        ano_inicio = v.attrib['ANO-INICIO']
-                        ano_fim = v.attrib['ANO-FIM'] or "Atual"
-                        natureza = v.attrib['NATUREZA']
-                        entra = True
-
-                        #print(i, 0, coord)
-                        print(i, 1, projeto)
-                        entra = True
-
-                        financiamento = []
-                        coordenador = ""
-
-                    if entra and v.tag == 'INTEGRANTES-DO-PROJETO':
-                        integrantes.append(v.attrib['NOME-COMPLETO'])
-
-                        if v.attrib['FLAG-RESPONSAVEL'] == "SIM" and coordenador == "":
-                            coordenadores.append(v.attrib['NOME-COMPLETO'])
-
-                    if entra and v.tag == 'FINANCIADOR-DO-PROJETO':
-                        financiamento.append(v.attrib['NOME-INSTITUICAO'])
-
-            if projeto:
-                prod = "\n".join(projeto)
-                #save = Titulo(projeto)
-                print(prod)
-
-            print(integrantes)
-            print(coord)
-            print(projeto)
-            print(ano_inicio)
-            print(natureza)
-            print(coordenadores)
-            print(financiamento)
-            print(integrantes)
-
-            if coord and projeto and ano_inicio and natureza and coordenadores and financiamento and integrantes:
-
-
-                save = Projetos(coord,projeto,ano_inicio,natureza,
-                                coordenador,financiamento, integrantes)
-                db.session.add(save)
-                db.session.commit()
-                if db.session.commit():
-                    flash("Salvo com sucesso!")
-                    redirect("projetos.html")
-                else:
-                    flash("Não foi possível salvar")
-                    redirect("index.html")
-            '''
-    # print(dados)
-    '''
-    if coord and projeto and ano_inicio and natureza and coordenadores and fin and integra:
-        s = Projetos(coord, projeto, ano_inicio, natureza,
-                        coordenadores, integra, fin)
-        #print(s)
-        
-        try:
-            db.session.add(s)
-            db.session.commit()
-            flash("Salvo com sucesso!")
-        except:
-            flash("Não foi possível salvar")
-        '''
                 
-    
+                c = db.cursor()
+                
+                data  = """ insert into projetos (nome,titulo, ano_inicio, natureza, coordenador, financiamento, integrantes)
+                                VALUES (%s,%s,%s,%s,%s,%s,%s)"""
+                                            
+                c.execute(data,(coord,projeto,ano_inicio,natureza,coordenadores, fin , integra))
+                db.commit()
+                c.close()
+        
     return render_template("projetos.html")
 
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ models/lattes4web.sql
@@ -0,0 +1,12 @@
+
+create table projetos(
+id int(11) not null auto_increment,
+nome varchar(255) not null,
+titulo text not null,
+ano_inicio varchar(4) not null,
+natureza varchar(100) not null,
+coordenador varchar(255),
+financiamento text,
+integrantes text,
+primary key (id)
+);

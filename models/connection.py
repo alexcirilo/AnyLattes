05a8a83462f5ad191df213753e662fbc0c31cd43
alexcirilo <c.alex.cirilo@gmail.com>
2022-11-22 00:05:10 -0300

    correcao conexao

--- models/connection.py
+++ models/connection.py
@@ -5,20 +5,20 @@ import mysql.connector
 '''
 heroku access
 '''
-'''
+
 user= 'b96e08051c345f'
 pwd= '2503c6ba'
 host= 'us-cdbr-east-06.cleardb.net'
 database= 'heroku_34fb507d853ce4f'
-'''
+
 '''
 local access
-'''
+
 user = 'root'
 pwd = 'Qwer@1234'
 host = 'localhost'
 database = 'lattes4web'
-
+'''
 def conexao():
     try:
         db = mysql.connector.connect(user=user,password= pwd,host=host, database=database)

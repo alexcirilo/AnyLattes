c72661a140cf8f1b10ebb5471c9fdf70d84db3c9
alexcirilo <c.alex.cirilo@gmail.com>
2022-11-26 22:22:11 -0300

    retirada do debug no app.py

--- app.py
+++ app.py
@@ -141,4 +141,4 @@ def listar():
     return render_template("teste.html", listar = listar, totalNotas = totalNotas, contadorEstratos = contadorEstratos)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run()

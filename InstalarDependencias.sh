df1b3667f41b1f71b52d4d2c40d5a351d31cd4c6
alexcirilo <c.alex.cirilo@gmail.com>
2023-03-26 23:25:04 -0300

    ajuste front e correção exibição (falta ajustar model)

---
+++ InstalarDependencias.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Instalando dependencias"
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install python3
+sudo apt install python3-pip
+sudo apt install python3-venv
+pip3 install Flask
+python3 -m venv venv
+source venv/bin/activate
+pip3 install xlrd
+pip3 install -r requirements.txt
+pip install PyPDF2
+pip install matplotlib
+
+
+echo "Dependencias Instaladas"
+
